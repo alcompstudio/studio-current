943e9b1ad90379829d2ec9d24211743993c9dd58
alcompstudio <alcompstudio@gmail.com>
2025-05-15 01:35:47 +0300

    feat: add temporary test component for docs system

---
+++ src/components/temp/TemporaryTestComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Это временный тестовый компонент для проверки системы документации.
+ * Его можно безопасно удалить после тестирования.
+ */
+const TemporaryTestComponent = () => {
+  return (
+    <div>
+      <p>Это временный тестовый компонент.</p>
+    </div>
+  );
+};
+
+export default TemporaryTestComponent;
